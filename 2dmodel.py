e5f04c9da4b21432be1f62c8547d0b33a61b68a2
ViggyC <vich7304@colorado.edu>
2022-04-04 20:33:11 -0600

    initial random walk

--- 2dmodel.py
+++ 2dmodel.py
@@ -1,11 +1,23 @@
 ##################2D - Model of Basking Shark Levy Walk ################
 from turtle import *
+import random
+import numpy as np
 color('red', 'yellow')
+turtle = Turtle()
+tp = turtle.position()
+
 begin_fill()
 while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
+    turtle.right(random.randint(0,360))
+    distance = random.randint(25,100)
+    # r = random.randint(0,100)
+    # if r<1:
+    #     distance = random.randint(25,100)
+    # else:
+    #     distance = 2
+
+
+    turtle.forward(distance)
+
 end_fill()
 done()
