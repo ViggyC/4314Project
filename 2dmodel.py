ae1ce70e793b3f8ce60c7c9fb997244f1e1511e6
bentonhaensel <60154240+bentonhaensel@users.noreply.github.com>
2022-04-19 16:51:18 -0600

    Finish Sesacape and Patch Generator

--- 2dmodel.py
+++ 2dmodel.py
@@ -115,33 +115,103 @@ while i<5:
 #end_fill()
 done()
 
+def create_seascape_uniform(patch_length, patch_count, seascape_length, seascape_width):
+    if(seascape_length % patch_length != 0 or seascape_width % patch_length !=0):
+        raise Exception("Seascape length and width must both be dividable by patch length")
+    seascape = np.zeros((seascape_length, seascape_width))
+    i = 0
+    x = random.randint(0, seascape_width-100)
+    y = random.randint(0, seascape_length-100)
+    while(i < patch_count):
+        patch = generate_patch(100)
+        #print(patch.shape, x, y)
+        y2 = 0
+        for y1 in range(y, y + patch_length): #This is to replace the values with the values generated from the patch generator
+            x2 = 0
+            for x1 in range(x, x+patch_length):
+                if(x1 >= seascape_width): #Torus properties
+                    x1 -= seascape_width
+                if(y1 >= seascape_length):
+                    y1 -= seascape_length
+                seascape[y1][x1] = seascape[y1][x1] + patch[y2][x2] #Update cell value
+                x2+=1
+            y2+=1
+        stepx = random.randint(0, seascape_width-100)
+        stepy = random.randint(0, seascape_length-100)
+        newposy = int(y + stepy)
+        newposx = int(x + stepx)
+        if(newposy >= seascape_length - 1):
+            newposy = newposy - seascape_length - 1
+        if(newposy < 0):
+            newposy = newposy + seascape_length - 1
+        if(newposx >= seascape_width - 1):
+            newposx = newposx - seascape_width - 1
+        if(newposx < 0):
+            newposx = newposx + seascape_width - 1
+        y = newposy
+        x = newposx
+        i += 1
+        #print(seascape)
+    return seascape
+
+def create_seascape_levy(patch_length, patch_count, seascape_length, seascape_width):
+    if(seascape_length % patch_length != 0 or seascape_width % patch_length !=0):
+        raise Exception("Seascape length and width must both be dividable by patch length")
+    seascape = np.zeros((seascape_length, seascape_width))
+    i = 0
+    x = random.randint(0, seascape_width-1)
+    y = random.randint(0, seascape_length-1)
+    a = np.array(np.concatenate((range(-2500,-50), range(50,2500)))) #concat range of min and max values + and -
+    prob = levy.levy(a, 1, 0, mu=2) #get probability distribution for walk
+    xsteps = [random.choices(a, weights=prob) for x in range(patch_count)]
+    ysteps = [random.choices(a, weights=prob) for x in range(patch_count)]
+    while(i < patch_count):
+        patch = generate_patch(patch_length)
+        print(patch.shape, x, y)
+        y2 = 0
+        for y1 in range(y, y + patch_length): #This is to replace the values with the values generated from the patch generator
+            x2 = 0
+            for x1 in range(x, x+patch_length):
+                if(x1 >= seascape_width): #Torus properties
+                    x1 -= seascape_width
+                if(y1 >= seascape_length):
+                    y1 -= seascape_length
+                seascape[y1][x1] = seascape[y1][x1] + patch[y2][x2] #Update cell value
+                x2+=1
+            y2+=1
+        stepx = xsteps[i][0]
+        stepy = ysteps[i][0]
+        newposy = int(y + stepy)
+        newposx = int(x + stepx)
+        if(newposy >= seascape_length - 1):
+            newposy = newposy - seascape_length - 1
+        if(newposy < 0):
+            newposy = newposy + seascape_length - 1
+        if(newposx >= seascape_width - 1):
+            newposx = newposx - seascape_width - 1
+        if(newposx < 0):
+            newposx = newposx + seascape_width - 1
+        y = newposy
+        x = newposx
+        i += 1
+        #print(seascape)
+    return seascape
+    
 def generate_patch(patch_length):
     patch = np.zeros((patch_length,patch_length))
-    maxstep = 5
-    minstep = 1
     req_area = (patch_length**2)
     big_l = int(req_area**.5)
-    pos=(50,50)
     hit = 0
     while(hit < req_area):
+        pos=(50,50)
         l = big_l
-        steps = levy_stable.rvs(alpha=1, beta=0, size=(big_l,2))
+        a = np.array(np.concatenate((range(int(-patch_length/4),0), range(0,int(patch_length/4))))) #concat range of min and max values + and -
+        prob = scipy.stats.norm(0, 7).pdf(np.concatenate((range(int(-patch_length/4),0), range(0,int(patch_length/4))))) #get probability distribution for walk
+        xsteps = [random.choices(a, weights=prob) for x in range(big_l)] #get x displacement
+        ysteps = [random.choices(a, weights=prob) for y in range(big_l)] #get y displacement
         for i in range(big_l):
-            #print(steps[i])
-            if abs(steps[i][0]) > maxstep:
-                stepx = maxstep
-            elif abs(steps[i][0]) < minstep:
-                stepx = minstep
-            else:
-                stepx = steps[i][0]
-            if abs(steps[i][1]) > maxstep:
-                stepy = maxstep
-            elif abs(steps[i][1]) < minstep:
-                stepy = minstep
-            else:
-                stepy = steps[i][1]
-            newposy = int(pos[0] + stepy)
-            newposx = int(pos[1] + stepx)
+            newposy = int(pos[0] + ysteps[i][0])
+            newposx = int(pos[1] + xsteps[i][0])
             if(newposy >= 100):
                 newposy = newposy - 100
             elif(newposy < 0):
@@ -156,57 +226,6 @@ def generate_patch(patch_length):
                 hit += 1
             l -= 1
     return patch
-
-def create_seascape(patch_length, patch_count, seascape_length, seascape_width):
-    if(seascape_length % patch_length != 0 or seascape_width % patch_length !=0):
-        raise Exception("Seascape length and width must both be dividable by patch length")
-    seascape = np.zeros((seascape_length, seascape_width))
-    i = 0
-    x = 0
-    y = 0
-#     minstep = 1
-#     maxstep = 500
-#     steps = levy_stable.rvs(alpha=1, beta=0, size=(patch_count,2))
-    while(i < patch_count):
-        patch = generate_patch(100)
-        print(patch.shape, x, y)
-        y2 = 0
-        for y1 in range(y, y + patch_length):
-            x2 = 0
-            for x1 in range(x, x+patch_length):
-                if(x1 >= seascape_width):
-                    x1 -= seascape_width
-                if(y1 >= seascape_length):
-                    y1 -= seascape_length
-                seascape[y1][x1] = seascape[y1][x1] + patch[y2][x2]
-                x2+=1
-            y2+=1
-        x = random.randint(0, seascape_width-100)
-        y = random.randint(0, seascape_length-100)
-        i += 1
-#         if abs(steps[i][0]) > maxstep:
-#             stepx = math.copysign(maxstep, steps[i][0])
-#         elif abs(steps[i][0]) < minstep:
-#             stepx = math.copysign(minstep, steps[i][0])
-#         else:
-#             stepx = steps[i][0]
-#         if abs(steps[i][1]) > maxstep:
-#             stepy = math.copysign(maxstep, steps[i][1])
-#         elif abs(steps[i][1]) < minstep:
-#             stepy = math.copysign(minstep, steps[i][1])
-#         else:
-#             stepy = steps[i][1]
-#         newposy = int(y + stepy)
-#         newposx = int(x + stepx)
-#         if(newposy >= 4900):
-#             newposy = newposy - 4900
-#         elif(newposy < 0):
-#             newposy = newposy + 4900
-#         if(newposx >= 2400):
-#             newposx = newposx - 2400
-#         elif(newposx < 0):
-#             newposx = newposx + 2400
-#         y = newposy + patch.shape[1]
-#         x = newposx + patch.shape[0]
-        #print(seascape)
-    return seascape
+        
+            
+            
